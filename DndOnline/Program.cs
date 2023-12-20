8e123313d2249d3245036a12d43fb1dd0eb31876
flores666 <zatworld3@gmail.com>
2023-12-20 23:30:14 +0300

    Базовые стили + рефакторинг

--- DndOnline/Program.cs
+++ DndOnline/Program.cs
@@ -57,6 +57,6 @@ app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=SignIn}/{id?}");
 
 app.Run();
