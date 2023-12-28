5854c9a24297c21f46456959194a9bd7bf957869
flores666 <zatworld3@gmail.com>
2023-12-28 21:39:14 +0300

    Домашняя страница со списком всех лобби, страница лобби

--- DndOnline/Controllers/HomeController.cs
+++ DndOnline/Controllers/HomeController.cs
@@ -1,14 +1,30 @@
-﻿using DndOnline.Extensions;
-using Microsoft.AspNetCore.Authorization;
+﻿using DndOnline.DataAccess;
+using DndOnline.Extensions;
+using DndOnline.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DndOnline.Controllers;
 
 [Auth]
 public class HomeController : Controller
 {
+    private ILobbyService _lobbyService;
+    
+    public HomeController(ILobbyService lobbyService)
+    {
+        _lobbyService = lobbyService;
+    }
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        ViewBag.Title = "Домашняя страница";
+        ViewData["Name"] = HttpContext.User.Identity.Name;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var lobbies = _lobbyService.GetLobbies();
+        return View(lobbies);
     }
 }
---
+++ DndOnline/Controllers/LobbyController.cs
@@ -0,0 +1,45 @@
+﻿using DndOnline.Extensions;
+using DndOnline.Models;
+using DndOnline.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DndOnline.Controllers;
+
+[Auth]
+public class LobbyController : Controller
+{
+    private readonly ILobbyService _lobbyService;
+
+    public LobbyController(ILobbyService lobbyService)
+    {
+        _lobbyService = lobbyService;
+    }
+    
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        ViewBag.Title = "Лобби";
+    }
+
+    public IActionResult Index(Guid id)
+    {
+        var lobby = _lobbyService.GetLobby(id);
+        return View(lobby);
+    }
+
+    [HttpPost]
+    public IActionResult CreateLobby(LobbyFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            _lobbyService.CreateLobby(model);
+        }
+        else
+        {
+            ViewBag.Alert = "Ошибка создания лобби";
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+}
