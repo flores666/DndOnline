501d98ba89b3287cd0e15fdef3925a8fabdbbdaa
flores666 <regenerationdream@gmail.com>
2024-01-02 23:27:38 +0300

    3d эффект для карточек

--- DndOnline/wwwroot/js/site.js
+++ DndOnline/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".lobby-item");
+    const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
+    const THRESHOLD = 10;
 
-// Write your JavaScript code.
+    function handleHover(e) {
+        const { clientX, clientY, currentTarget } = e;
+        const { clientWidth, clientHeight, offsetLeft, offsetTop } = currentTarget;
+
+        const horizontal = (clientX - offsetLeft) / clientWidth;
+        const vertical = (clientY - offsetTop) / clientHeight;
+        const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+        const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+        currentTarget.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+    }
+
+    function resetStyles(e) {
+        e.currentTarget.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+    }
+
+    if (!motionMatchMedia.matches) {
+        cards.forEach(function (card) {
+            card.addEventListener("mousemove", handleHover);
+            card.addEventListener("mouseleave", resetStyles);
+        });
+    }
+});
