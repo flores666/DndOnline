92831ef6782dc15847a7db126438f01e2da0a506
flores666 <regenerationdream@gmail.com>
2024-01-14 21:51:05 +0300

    стили для 3d карточек

--- DndOnline/wwwroot/js/site.js
+++ DndOnline/wwwroot/js/site.js
@@ -1,30 +1,5 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll(".lobby-item");
-    const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
-    const THRESHOLD = 10;
-
-    function handleHover(e) {
-        const {clientX, clientY, currentTarget} = e;
-        const {clientWidth, clientHeight, offsetLeft, offsetTop} = currentTarget;
-
-        const horizontal = (clientX - offsetLeft) / clientWidth;
-        const vertical = (clientY - offsetTop) / clientHeight;
-        const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
-        const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
-
-        currentTarget.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
-    }
-
-    function resetStyles(e) {
-        e.currentTarget.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
-    }
-
-    if (!motionMatchMedia.matches) {
-        cards.forEach(function (card) {
-            card.addEventListener("mousemove", handleHover);
-            card.addEventListener("mouseleave", resetStyles);
-        });
-    }
+    handleCard3d();
 
     $('#lobby_search').on('input', function () {
         let input = $('#lobby_search').val();
@@ -71,3 +46,31 @@
         });
     }
 });
+
+function handleCard3d() {
+    const cards = document.querySelectorAll(".card3d");
+    const motionMatchMedia = window.matchMedia("(prefers-reduced-motion)");
+    const THRESHOLD = 5;
+    
+    function handleHover(e) {
+        const {clientX, clientY, currentTarget} = e;
+        const {clientWidth, clientHeight, offsetLeft, offsetTop} = currentTarget;
+        const horizontal = (clientX - offsetLeft) / clientWidth;
+        const vertical = (clientY - offsetTop) / clientHeight;
+        const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+        const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+
+        currentTarget.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+    }
+
+    function resetStyles(e) {
+        e.currentTarget.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+    }
+
+    if (!motionMatchMedia.matches) {
+        cards.forEach(function (card) {
+            card.addEventListener("mousemove", handleHover);
+            card.addEventListener("mouseleave", resetStyles);
+        });
+    }
+}
