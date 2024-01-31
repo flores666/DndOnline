bed3b223635f37c5804f697c43e00d5c13be1253
flores666 <regenerationdream@gmail.com>
2024-01-31 16:51:11 +0300

    Переименован интерфейс файлового сервиса

--- DndOnline/Services/Interfaces/IFIleService.cs
+++
@@ -1,8 +0,0 @@
-﻿using DndOnline.Models;
-
-namespace DndOnline.Services.Interfaces;
-
-public interface IFIleService
-{
-    public Task<ResponseModel> Save(IFormFile file, string type);
-}
---
+++ DndOnline/Services/Interfaces/IFileService.cs
@@ -0,0 +1,8 @@
+﻿using DndOnline.Models;
+
+namespace DndOnline.Services.Interfaces;
+
+public interface IFileService
+{
+    public Task<ResponseModel> SaveAsync(IFormFile file, string type);
+}
